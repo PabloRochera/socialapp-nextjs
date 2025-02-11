a55c55a00a6a0906d3381a5ff16e8a6a3566e756
Pablo Rochera Garcia <pablorocheragarcia@gmail.com>
2025-02-11 17:37:25 +0100

    errorInvalid

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import PostList from "@/app/ui/post-list";
+import { getLikes, getPosts } from "./lib/data";
+import { auth0 } from "./lib/auth0";
 
-export default function Home() {
+export default async function Home() {
+  const user_id = (await auth0.getSession()).user.user_id;
+
+  // TODO: lanzar las dos consultas a la vez
+  const posts = await getPosts();    
+  const likes = await getLikes(user_id);
 
 
-
-  return (
-    <>
-      <PostList />
-    </>
-  );
+  return <PostList user_id={user_id} posts={posts} likes={likes} />
 }
